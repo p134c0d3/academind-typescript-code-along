ed5c531256c7c103d9878039887607eca09900f2
John P <70922864+p134c0d3@users.noreply.github.com>
2023-09-30 11:14:57 -0500

    Add in Recipe Item Array
    - Created the Recipe model to use in our Recipe List component
    - Used ngFor to display Recipes dynamically

--- angular-basics-course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular-basics-course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is a simply a test',
+      'https://www.simplyrecipes.com/thmb/KRw_r32s4gQeOX-d07NWY1OlOFk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1c-c2b1885d27d4481c9cfe6f6286a64342.jpg'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is a simply a test',
+      'https://www.simplyrecipes.com/thmb/KRw_r32s4gQeOX-d07NWY1OlOFk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1c-c2b1885d27d4481c9cfe6f6286a64342.jpg'
+    ),
+  ];
 
+  constructor() {}
+
+  ngOnInit() {}
 }
